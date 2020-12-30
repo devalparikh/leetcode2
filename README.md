79ccbb2f3b8782ed890a9316cf20c617d1376d8b
Deval Parikh <devalpp@gmail.com>
2020-12-30 17:23:00 -0500

    next permutation in linear time

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Enumerate Prime Numbers | Easy | Arrays | :white_check_mark:
 [ValideSudoku](https://leetcode.com/problems/valid-sudoku/) | Medium | Arrays, Hash Table | :black_square_button:
 [SpiralMatrix](https://leetcode.com/problems/spiral-matrix/) | Medium | Arrays | :white_check_mark:
 [CountSubarraysThatSumToK](https://leetcode.com/problems/subarray-sum-equals-k/) | Medium | Arrays, Hash Table | :white_check_mark:
-[NextPermutation](https://leetcode.com/problems/next-permutation/) | Hard | Arrays | :black_square_button:
+[NextPermutation](https://leetcode.com/problems/next-permutation/) | Hard | Arrays | :white_check_mark:
+PatternMatching | Easy | Strings | :black_square_button:
 
--- src/Array/NextPermutation.java
+++ src/Array/NextPermutation.java
@@ -5,4 +5,67 @@
 package Array;
 
 public class NextPermutation {
+    public static void main(String args[]) {
+
+    }
+
+    // O(n) time, O(1) space
+    public void nextPermutation(int[] nums) {
+
+        // Find target - the item to the left of a strictly decreasing tail section (increasing from the end)
+        int i_target = nums.length-2;
+
+        for(int i = nums.length-2; i >= 0; i--) {
+            if(nums[i] < nums[i+1]) {
+                // Found target
+                break;
+            }
+            i_target = i-1;
+        }
+
+        if(i_target < 0) {
+            // Reverse whole thing since entire array is strictly decreasing
+            reverse(nums, 0);
+            return;
+        }
+
+        // O(n)
+        // Find newTarget - the next greatest number from the target within the strictly decreasing tail section
+        int i_newTarget = i_target+1; // (min but larger than target)
+        for(int i = i_target+1; i < nums.length; i++) {
+            // Could be <= than the previous newTarget to swap with last instance of that value
+            if(nums[i] <= nums[i_newTarget] && nums[i] > nums[i_target])  {
+                i_newTarget = i;
+            }
+        }
+
+        // Swap target and newTarget
+        swap(nums, i_target, i_newTarget);
+
+        // Reverse strictly decreasing tail section
+        reverse(nums, i_target+1);
+
+    }
+
+    // O(n)
+    // Reverse array starting from a start index
+    private void reverse(int[] nums, int start_index) {
+
+        int i = start_index;
+        int j = nums.length - 1;
+
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    // O(1)
+    // Swap in place
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
